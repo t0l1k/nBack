fbfa27574cd5ae748d002712defb2d4f18238bf2
Anatoli Tjushka <t0l1k@mail.ru>
2022-07-18 16:55:24 +0300

    refactor package game

--- main.go
+++ main.go
@@ -4,11 +4,12 @@ import (
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/t0l1k/nBack/game"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
-	if err := ebiten.RunGame(NewGame()); err != nil {
+	if err := ebiten.RunGame(game.NewGame()); err != nil {
 		log.Fatal(err)
 	}
 }
